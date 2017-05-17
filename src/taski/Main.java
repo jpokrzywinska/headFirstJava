d3ead3b36e62f6107257ee79dd3bd63e3f6b9389
justyna <j.pokrzywinska@gmail.com>
2017-05-17 20:22:28 +0200

    added new task

---
+++ src/taski/Main.java
@@ -0,0 +1,21 @@
+package taski;
+
+/**
+ * Created by jpokr on 2017-05-17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+   // new String("abc").equals("abc");
+    //new String("abc") =="abc";
+
+    //new String("abc") ==new String("abc");
+
+        System.out.println("abc"=="abc");
+        System.out.println(new String("abc").equals("abc"));
+        System.out.println(new String("abc") =="abc");
+        System.out.println(new String("abc") ==new String("abc"));
+}
+}
