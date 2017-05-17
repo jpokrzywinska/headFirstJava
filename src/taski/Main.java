08affba2d3478a461d4af6391072441b0df455e8
justyna <j.pokrzywinska@gmail.com>
2017-05-17 20:24:08 +0200

    new

--- src/taski/Main.java
+++ src/taski/Main.java
@@ -8,14 +8,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-   // new String("abc").equals("abc");
-    //new String("abc") =="abc";
-
-    //new String("abc") ==new String("abc");
-
-        System.out.println("abc"=="abc");
+        System.out.println("abc" == "abc");
         System.out.println(new String("abc").equals("abc"));
-        System.out.println(new String("abc") =="abc");
-        System.out.println(new String("abc") ==new String("abc"));
-}
+        System.out.println(new String("abc") == "abc");
+        System.out.println(new String("abc") == new String("abc")); // tworzac dwa nowe obiekty bedzie false
+    }
 }
